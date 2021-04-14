07e6ccb17eeba56238a846870f385199ce98d080
petergal3 <peterga3@gmail.com>
2021-04-14 17:59:08 +0200

    1. feladat kész

---
+++ Verseny/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verseny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] teljes = File.ReadAllLines("balkezesek.csv");
+
+            int versenyzokDb = teljes.Count() - 1;
+            Console.WriteLine($"Feladat 1: {versenyzokDb} db versenyzőről van adatunk.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
