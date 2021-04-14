2136ab47736ddbfcc6d61417b1eaa329f0b0f83f
petergal3 <peterga3@gmail.com>
2021-04-14 18:34:14 +0200

    3. feladat kész

--- Verseny/Program.cs
+++ Verseny/Program.cs
@@ -9,6 +9,8 @@ namespace Verseny
 {
     class Program
     {
+        public static List<Versenyzo> versenyzok = new List<Versenyzo>();
+        public static List<Versenyzo> idoszakElsök = new List<Versenyzo>();
         static void Main(string[] args)
         {
             string[] teljes = File.ReadAllLines("balkezesek.csv");
@@ -16,8 +18,56 @@ namespace Verseny
             int versenyzokDb = teljes.Count() - 1;
             Console.WriteLine($"Feladat 1: {versenyzokDb} db versenyzőről van adatunk.");
 
+
+            
+            versenyzokDb = versenyzokDb - 1;
+            for (int i = 1; i < versenyzokDb; i++)
+            {
+                versenyzok.Add(new Versenyzo(teljes[i]));
+            }
+            
+            Console.WriteLine("\nFeladat 2:");
+            PalyaraLeptekEloszorEvben("1980.01.01", "1981.01.01");
+
+            Console.WriteLine("\nFeladat 3 (ehhez adj meg egy nevet kérlek aki játszott a megadott időszakban):");
+            string nev = Console.ReadLine();
+            NevVizsgalat(nev);
+
             Console.ReadLine();
 
+        }
+
+        public static void PalyaraLeptekEloszorEvben(string idöszakEleje, string idöszakvege) 
+        {
+            Console.WriteLine($"Első alkalommal volt pályán:");
+            DateTime idöszakElejedatum = DateTime.Parse(idöszakEleje);
+            DateTime idöszakvegedatum = DateTime.Parse(idöszakvege);
+            foreach (var item in versenyzok)
+            {
+                if (item.elso > idöszakElejedatum && idöszakvegedatum > item.elso)
+                {
+                    idoszakElsök.Add(item);
+                    Console.WriteLine(item.nev);
+                }
+            }
+        }
+
+        public static void NevVizsgalat(string nev) 
+        {
+            bool nemTalalt = true;
+            foreach (var item in idoszakElsök)
+            {
+                if (item.nev == nev)
+                {
+                    Console.WriteLine(item.magassag);
+                    nemTalalt = false;
+                }
+               
+            }
+            if (nemTalalt)
+            {
+                Console.WriteLine("Hibás adat");
+            }
         }
     }
 }
--- Verseny/Versenyzo.cs
+++ Verseny/Versenyzo.cs
@@ -8,6 +8,26 @@ namespace Verseny
 {
     class Versenyzo
     {
+        public string nev { get; set; }
+        public DateTime elso { get; set; }
+        public DateTime utolso { get; set; }
+        public int suly { get; set; }
+        public int magassag { get; set; }
+
+        public Versenyzo(string adatok) 
+        {
+            string[] adatokDarabolt = adatok.Split(';');
+            this.nev = adatokDarabolt[0];
+            this.elso = Convert.ToDateTime(adatokDarabolt[1]);
+            this.utolso = Convert.ToDateTime(adatokDarabolt[2]);
+            this.suly = Convert.ToInt32(adatokDarabolt[3]);
+            this.magassag = Convert.ToInt32(adatokDarabolt[4]);
+        }
+
+        public override string ToString()
+        {
+            return $"Név: {nev}, első: {elso}, utolsó: {utolso}, súly: {suly}, magasság: {magassag}";
+        }
 
     }
 }
